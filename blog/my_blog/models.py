33c81cc3c75663e2487b10fcf70de7d3bfad58e2
danielsind <danielsindet@gmail.com>
2023-05-30 11:14:23 +0300

    Created model Post ad a string finction and get absolute function for a landing page

--- blog/my_blog/models.py
+++ blog/my_blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse 
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -8,6 +9,12 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-   
+    def __str__(self):
+            return self.title
+
+    def get_absolute_url(self):
+            return reverse('post-detail', kwargs={'pk': self.pk})
+
+
 
 
