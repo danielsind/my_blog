4f070cafc2a43c590c14599e8276b2ebe4c74a8b
danielsind <danielsindet@gmail.com>
2023-05-30 11:15:47 +0300

    Defined all the paths for the CRUD functions

--- blog/my_blog/urls.py
+++ blog/my_blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+from .views import PostListView, PostDetailView, PostCreate, PostUpdate, PostDeleteView
 
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>', PostDetailView.as_view(), name = 'post-detail'),
+    path('post/<int:pk>/update', PostUpdate.as_view(), name = 'post-update'),
+    path('post/new', PostCreate.as_view(), name = 'post-create'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name = 'post-delete'),
 ]
