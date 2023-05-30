a4333889987f6b597889db99b417e572121185ca
danielsind <danielsindet@gmail.com>
2023-05-30 11:15:15 +0300

    Created all the CRUD functins and View functions needed

--- blog/my_blog/views.py
+++ blog/my_blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {
@@ -10,3 +11,46 @@ def home(request):
 
 def about(request):
     return render(request, 'my_blog/about.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'my_blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreate(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdate(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+        model = Post
+        fields = ['title', 'content']
+
+        def form_valid(self, form):
+            form.instance.author = self.request.user
+            return super().form_valid(form)
+        
+
+        def test_func(self):
+             post = self.get_object()
+             if self.request.user == post.author:
+                  return True
+             return False
+             
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    model = Post
+
+    success_url = '/'
+
+    def test_func(self):
+             post = self.get_object()
+             if self.request.user == post.author:
+                  return True
+             return False
